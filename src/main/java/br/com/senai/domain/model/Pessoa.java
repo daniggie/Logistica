3ed697f0719b9438dc00094a4ca66cfd10ebb221
daniggie <dani.gross53@gmail.com>
2021-05-27 07:31:02 -0300

    Novo projeto

---
+++ src/main/java/br/com/senai/domain/model/Pessoa.java
@@ -0,0 +1,41 @@
+package br.com.senai.domain.model;
+
+public class Pessoa {
+
+    private long id;
+    private String nome;
+    private String email;
+    private String telephone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
