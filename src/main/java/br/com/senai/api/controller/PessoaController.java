96841531542b68ba21d79cf5aeb5aaa8583ef20c
daniggie <dani.gross53@gmail.com>
2021-05-28 10:03:14 -0300

    atualizações 1.0

--- src/main/java/br/com/senai/api/controller/PessoaController.java
+++ src/main/java/br/com/senai/api/controller/PessoaController.java
@@ -1,33 +1,55 @@
 package br.com.senai.api.controller;
 
 import br.com.senai.domain.model.Pessoa;
+import br.com.senai.domain.repository.PessoaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
 
-    @GetMapping("/pessoas")
+    private PessoaRepository pessoaRepository;
+
+    @GetMapping
     public List<Pessoa> listar(){
-        Pessoa pessoa1 = new Pessoa(
-                1L,
-                "Daniela",
-                "dani@gmail.com",
-                "(47)91234-5678"
-        );
-
-        Pessoa pessoa2 = new Pessoa(
-                2L,
-                "Michel",
-                "Michel@gmail.com",
-                "(47)98765-4321"
-        );
+        return pessoaRepository.findAll();
+    }
 
 
-        return Arrays.asList(pessoa1, pessoa2);
+    @GetMapping("/nome/{pessoaNome}")
+    public List<Pessoa> listarPorNome(@PathVariable String pessoaNome){
+        return pessoaRepository.findByNome(pessoaNome);
+    }
+
+    @GetMapping("/nome/containing/{nomeContaining}")
+    public List<Pessoa> listarNomeContaining(@PathVariable String nomeContaining){
+        return pessoaRepository.findByNomeContaining(nomeContaining);
+    }
+
+    @GetMapping("{pessoaId}")
+    public ResponseEntity<Pessoa> buscar(@PathVariable Long pessoaId){
+//        Optional<Pessoa> pessoa = pessoaRepository.findById(pessoaId);
+//
+//        if (pessoa.isPresent()) {
+//            return ResponseEntity.ok(pessoa.get());
+//        }
+//        return ResponseEntity.notFound().build();
+    return pessoaRepository.findById(pessoaId)
+        .map(pessoa-> ResponseEntity.ok(pessoa))
+        .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/resources/db/migration/V0002__insertPessoa.sql
+++ src/main/resources/db/migration/V0002__insertPessoa.sql
@@ -1,2 +1,4 @@
 INSERT INTO pessoa
-VALUES (null, "Jairo", "jairo@email.com", "(47)98765-4321");
+VALUES (null, "Jairo", "jairo@email.com", "(47)98765-4321"),
+(null, "Larissa", "lari@email.com", "(47)11111-4321"),
+(null, "Diego", "diego@email.com", "(47)22222-4321");
