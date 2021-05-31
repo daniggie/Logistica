adb30097b529d88a2b2f711bccc03dfd2b1c0893
daniggie <dani.gross53@gmail.com>
2021-05-31 09:49:14 -0300

    31/05 finalizado implementações

--- src/main/java/br/com/senai/domain/model/Pessoa.java
+++ src/main/java/br/com/senai/domain/model/Pessoa.java
@@ -1,14 +1,22 @@
 package br.com.senai.domain.model;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.NotFound;
 
+import static lombok.AccessLevel.PRIVATE;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
 @Getter
 @Setter
 @Entity
@@ -18,11 +26,18 @@ public class Pessoa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String nome;
+    @NotBlank
+    @Size(max = 60)
+    String nome;
 
-    private String email;
+    @NotBlank
+    @Email
+    @Size(min = 5)
+    String email;
 
-    private String telephone;
+    @NotBlank
+    @Size(min = 14)
+    String telephone;
 
 
 }
