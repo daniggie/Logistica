44e7dfc3e6490076b2418ad9139cc4bf68b5ee8e
daniggie <dani.gross53@gmail.com>
2021-06-01 16:50:28 -0300

    Atualizações de conteúdo 01/06

---
+++ src/main/java/br/com/senai/domain/exception/NegocioException.java
@@ -0,0 +1,9 @@
+package br.com.senai.domain.exception;
+
+public class NegocioException extends RuntimeException{
+
+    public NegocioException(String message){
+        super(message);
+    }
+
+}
---
+++ src/main/java/br/com/senai/domain/service/PessoaService.java
@@ -0,0 +1,29 @@
+package br.com.senai.domain.service;
+
+import br.com.senai.domain.exception.NegocioException;
+import br.com.senai.domain.model.Pessoa;
+import br.com.senai.domain.repository.PessoaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PessoaService {
+
+    private PessoaRepository pessoaRepository;
+
+    @Transactional
+    public Pessoa cadastrar(Pessoa pessoa){
+        boolean emailValidation = pessoaRepository.findByEmail(pessoa.getEmail())
+                .isPresent();
+
+        if(emailValidation){
+            throw new NegocioException("E-mail já está sendo utilizado");
+        }
+
+        return pessoaRepository.save(pessoa);
+
+    }
+}
